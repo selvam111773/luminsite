7c3cadc197c83cf7c8628db6942fb74b22e4ee8c
subash chandra bose <b.subi68@live.com>
2013-12-22 19:14:03 +0530

    adding index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>php strom test page</title></head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Subash
+ * Date: 12/10/13
+ * Time: 10:40 AM
+ */
+class phpinfo{
+
+}
+phpinfo();
+?>
+</body>
+</html>
