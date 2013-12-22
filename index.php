ea64098d236c9723c2272535a8b76d37e854410e
subash chandra bose <b.subi68@live.com>
2013-12-22 19:25:42 +0530

    testFunction added

--- index.php
+++ index.php
@@ -2,12 +2,6 @@
 <head><title>php strom test page</title></head>
 <body>
 <?php
-/**
- * Created by PhpStorm.
- * User: Subash
- * Date: 12/10/13
- * Time: 10:40 AM
- */
 class phpinfo{
     function testFunction(){
         return 0;
