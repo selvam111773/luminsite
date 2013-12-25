b11f1ed3a95b9521b43b8965952837bdad34872e
subash chandra bose <b.subi68@live.com>
2013-12-25 21:57:09 +0530

    wire frame for the index page

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
     <link href="css/style.css" rel="stylesheet"/>
 </head>
 <body onload="callAlert()">
-some change
-<h1>big head</h1>
 <div class="header"></div>
+<div class="main-content">
+    <div class="focus200 inner_left">
+        <!-- Focus 200 content goes her  -->
+    </div>
+    <div class="application_links inner_middle">
+        <!-- application link goes here -->
+    </div>
+    <div class="sponsor inner_right">
+<!--        sponsors link goes here -->
+    </div>
+</div>
 <div class="footer"></div>
 <script src="js/index.js"></script>
 </body>
