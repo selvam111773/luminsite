ff7d4668f17d9cb976a2d93eecf19e0f43c31879
subash chandra bose <b.subi68@live.com>
2013-12-26 00:11:11 +0530

    div separation of the index content

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <html>
-<head><title>php strom test page</title>
+<head><title>Lumin Academy</title>
     <link href="css/style.css" rel="stylesheet"/>
 </head>
 <body>
-<div class="header">
-</div>
 <div class="main-content">
-    <div class="focus200 inner_left">
+    <div class="inner_left">
         some content in the inner left and its just a preview of focus 200
         <!-- Focus 200 content goes her  -->
     </div>
-    <div class="application_links inner_middle">
+    <div class="inner_middle">
         <!-- application link goes here -->
-        links for the application
+        <h3>links for application downloading</h3>
+        <div class="application">
+            <a href="#">IEEE application</a><br>
+            <a href="#">SRM admission application</a><br>
+            <a href="#">link for application 1</a><br>
+            <a href="#">link for application 2</a><br>
+            <a href="#">click to view full list</a>
+        </div>
     </div>
-    <div class="sponsor inner_right">
+    <div class="inner_right">
         <!-- sponsors link goes here -->
         links for the sponsors
     </div>
-</div>
-<div class="footer">
-
 </div>
 <script src="js/index.js"></script>
 </body>
