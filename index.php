2b361f2c277a2bf667f1875ccb01f489edc55237
subash chandra bose <b.subi68@live.com>
2013-12-25 23:00:51 +0530

    div stylesheet change

--- index.php
+++ index.php
@@ -2,20 +2,27 @@
 <head><title>php strom test page</title>
     <link href="css/style.css" rel="stylesheet"/>
 </head>
-<body onload="callAlert()">
-<div class="header"></div>
+<body>
+<div class="header">
+
+</div>
 <div class="main-content">
     <div class="focus200 inner_left">
+        some content in the inner left and its just a preview of focus 200
         <!-- Focus 200 content goes her  -->
     </div>
     <div class="application_links inner_middle">
         <!-- application link goes here -->
+        links for the application
     </div>
     <div class="sponsor inner_right">
-<!--        sponsors link goes here -->
+        <!-- sponsors link goes here -->
+        links for the sponsors
     </div>
 </div>
-<div class="footer"></div>
+<div class="footer">
+
+</div>
 <script src="js/index.js"></script>
 </body>
 </html>
