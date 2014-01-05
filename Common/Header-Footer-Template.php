623b92c955546ad6330ce0663d592377f1cdbd4a
subash68 <b.subi68@live.com>
2014-01-05 18:48:54 +0530

    moodify header

--- Common/Header-Footer-Template.php
+++ Common/Header-Footer-Template.php
@@ -1,33 +1,32 @@
 <html>
 <head>
-<link type="text/css" rel="stylesheet" href="css/Header-Footer.css"/>
-<link type="text/css" href="css/Header-Footer.css" rel="=stylesheet"/>
+    <link type="text/css" href="css/Header-Footer.css" rel="=stylesheet"/>
 </head>
 <div id="Header">
-  <div id="Title-Header">
+    <div id="Title-Header">
 
-    <div id="Title">
-        <img src="images/name.png">
-    </div>
-  </div>
-  <div id="Tab-Control">
-    <div id="tabs">
-      <div class="sub-tab">
-           Home
-       </div>
-        <div class="sub-tab">
-            About us
+        <div id="Title">
+            <img src="images/name.png">
         </div>
-      <div class="sub-tab">
-          Events
-      </div>
-      <div class="sub-tab">
-          Services
-      </div>
-      <div class="sub-tab">
-          Contact us
-      </div>
     </div>
+    <div id="Tab-Control">
+        <div id="tabs">
+            <div class="sub-tab">
+                Home
+            </div>
+            <div class="sub-tab">
+                About us
+            </div>
+            <div class="sub-tab">
+                Events
+            </div>
+            <div class="sub-tab">
+                Services
+            </div>
+            <div class="sub-tab">
+                Contact us
+            </div>
+        </div>
     </div>
 
 
